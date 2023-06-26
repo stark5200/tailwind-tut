fc8ea91f2a44e4fdb013f07e1377c7b685df6174
stark5200 <a.taha5200@gmail.com>
2023-06-26 20:47:12 +0200

    start of lesson 04

--- lesson02/tailwind.config.js
+++ lesson02/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratioo 3/2)' },
+        'tallscreen': { 'raw': '(min-aspect-ratioo 13/20)' },
+      }
+    },
   },
   plugins: [],
 }
