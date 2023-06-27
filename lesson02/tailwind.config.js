fceb168a7ebdf0a6afb7f2a679f123e96cd4ef6f
stark5200 <a.taha5200@gmail.com>
2023-06-27 04:24:27 +0200

    animation

--- lesson02/tailwind.config.js
+++ lesson02/tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
       screens: {
         'widescreen': { 'raw': '(min-aspect-ratioo 3/2)' },
         'tallscreen': { 'raw': '(min-aspect-ratioo 13/20)' },
-      }
+      }, 
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)'}, 
+          '80%': { transform: 'scaleY(1.2)'}, 
+          '100%': { transform: 'scaleY(1)'}, 
+        }, 
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      },
     },
   },
   plugins: [],
